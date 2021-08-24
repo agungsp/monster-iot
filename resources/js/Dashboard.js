b96a1d171953593dc8750bb5f6938fa652610e5f
Agung Setyo Pribadi <agungsetyopribadi8@gmail.com>
2021-08-24 13:56:13 +0700

    Simplify Dashboard js

--- resources/js/Dashboard.js
+++ resources/js/Dashboard.js
@@ -1,49 +1,127 @@
 import MqttSub from "./MqttSub";
-
 class Dashboard extends MqttSub {
+    configEvent = {};
+
     constructor() {
         super("103.31.39.42", 9001);
+        this.configEvent = {
+            "event/pintu": {
+                SP1: {
+                    name: "Pintu 1",
+                    state: ["Terbuka", "Tertutup"], // Index 0 = anomali, 1 = normal
+                },
+                SP2: {
+                    name: "Pintu 2",
+                    state: ["Terbuka", "Tertutup"],
+                },
+                MAG: {
+                    name: "Kunci Pintu",
+                    state: ["Tidak Terkunci", "Terkunci"],
+                },
+            },
+            "event/beban": {
+                PROX: {
+                    name: "Proximity",
+                    state: ["Tidak Aman", "Aman"],
+                },
+                LC: {
+                    name: "Berat Kontainer",
+                    state: 50,
+                },
+            },
+            "event/base": {
+                PB: {
+                    name: "Emergency Button",
+                    state: ["Bahaya", "Aman"],
+                },
+                RS: {
+                    name: "Kondisi Mesin",
+                    state: ["Mati", "Menyala"],
+                },
+                DRI: {
+                    name: "Driving Behaviour",
+                    state: ["Tidak Stabil", "Stabil"],
+                },
+                DRO: {
+                    name: "Drowsiness",
+                    state: ["Mengantuk", "Tidak Mengantuk"],
+                },
+                LAT: {
+                    name: "Latitude",
+                },
+                LON: {
+                    name: "Longitude",
+                },
+            },
+            except: ["LC", "LAT", "LON"],
+        };
     }
+
+    eventHandler(topic, data) {
+        for (let key in this.configEvent[topic]) {
+            if (this.configEvent["except"].indexOf(key) < 0) {
+                document.getElementById(key).innerHTML =
+                    this.configEvent[topic][key].state[data[key].value];
+                this.toastMessage.icon = data[key].value ? "error" : "success";
+                this.toastMessage.title =
+                    this.configEvent[topic][key].name +
+                    " " +
+                    this.configEvent[topic][key].state[data[key].value];
+                this.toastMessage.footer = "Ini untuk device";
+            } else {
+                document.getElementById(key).innerHTML = data[key].value;
+            }
+        }
+    }
+
     onMessageArrived(msg) {
         out_msg = "Message received " + msg.payloadString + "<br>";
+        console.log("Message received");
         let pesan = "" + msg.payloadString;
         let data = JSON.parse(pesan);
-        switch (msg.destinationName) {
-            case "/event/pintu":
-                document.getElementById("SP1").innerHTML =
-                    data.SP1 == "0" ? "Terbuka" : "Tertutup";
+        this.eventHandler(msg.destinationName, data);
+        // switch (msg.destinationName) {
+        //     case "/event/pintu":
+        //         // Pintu 1
+        //         document.getElementById("SP1").innerHTML =
+        //             data.SP1.value == "0" ? "Terbuka" : "Tertutup";
+        //         // End Pintu 1
 
-                document.getElementById("SP2").innerHTML =
-                    data.SP2 == "0" ? "Terbuka" : "Tertutup";
+        //         // Pintu 2
+        //         document.getElementById("SP2").innerHTML =
+        //             data.SP2.value == "0" ? "Terbuka" : "Tertutup";
+        //         // End Pintu 2
 
-                document.getElementById("MAG").innerHTML =
-                    data.MAG == "0" ? "Tidak Terkunci" : "Terkunci";
-                break;
+        //         // Kunci Pintu
+        //         document.getElementById("MAG").innerHTML =
+        //             data.MAG.value == "0" ? "Tidak Terkunci" : "Terkunci";
+        //         // End Pintu 2
+        //         break;
 
-            case "/event/beban":
-                document.getElementById("PROX").innerHTML =
-                    data.PROX == 0 ? "Tidak Aman" : "Aman";
+        //     case "/event/beban":
+        //         document.getElementById("PROX").innerHTML =
+        //             data.PROX.value == 0 ? "Tidak Aman" : "Aman";
 
-                document.getElementById("LC").innerHTML = data.LC;
-                break;
+        //         document.getElementById("LC").innerHTML = data.LC;
+        //         break;
 
-            case "/event/base":
-                document.getElementById("PB").innerHTML =
-                    data.PB == "0" ? "Bahaya" : "Aman";
+        //     case "/event/base":
+        //         document.getElementById("PB").innerHTML =
+        //             data.PB == "0" ? "Bahaya" : "Aman";
 
-                document.getElementById("RS").innerHTML =
-                    data.RS == "1" ? "Mati" : "Menyala";
+        //         document.getElementById("RS").innerHTML =
+        //             data.RS == "1" ? "Mati" : "Menyala";
 
-                document.getElementById("DRI").innerHTML =
-                    data.DRI == 0 ? "Stabil" : "Tidak Stabil";
+        //         document.getElementById("DRI").innerHTML =
+        //             data.DRI == 0 ? "Stabil" : "Tidak Stabil";
 
-                document.getElementById("DRO").innerHTML =
-                    data.DRO == 0 ? "Tidak Mengantuk" : "Mengantuk";
+        //         document.getElementById("DRO").innerHTML =
+        //             data.DRO == 0 ? "Tidak Mengantuk" : "Mengantuk";
 
-                document.getElementById("LAT").innerHTML = data.LAT;
-                document.getElementById("LON").innerHTML = data.LON;
-                break;
-        }
+        //         document.getElementById("LAT").innerHTML = data.LAT;
+        //         document.getElementById("LON").innerHTML = data.LON;
+        //         break;
+        // }
     }
 
     lockPintu() {
--- resources/js/MqttSub.js
+++ resources/js/MqttSub.js
@@ -1,10 +1,30 @@
+import Swal from "sweetalert2";
+
 class MqttSub {
     reconnectTimeout = 2000;
     mqtt = null;
+    toast = null;
+    toastMessage = null;
 
     constructor(host = "127.0.0.1", port = 1883) {
         this.host = host;
         this.port = port;
+        this.toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 5000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener("mouseenter", Swal.stopTimer);
+                toast.addEventListener("mouseleave", Swal.resumeTimer);
+            },
+        });
+        this.toastMessage = {
+            icon: "info",
+            title: "",
+            footer: "",
+        };
     }
 
     connect() {
@@ -18,17 +38,25 @@ class MqttSub {
             onFailure: this.onFailure,
         };
         this.mqtt.onMessageArrived = this.onMessageArrived;
-        this.mqtt.connect(options);
+        try {
+            this.mqtt.connect(options);
+        } catch (error) {
+            console.error(error.message);
+        }
     }
 
     onConnect() {
         console.log("Connected");
-        this.mqtt.subscribe("event/#");
+        this.mqtt.subscribe("/event/#");
     }
 
     onFailure(message) {
         console.log(`Connection Attempt to Host ${this.host} Failed`);
         setTimeout(this.connect, this.reconnectTimeout);
+    }
+
+    showNotification() {
+        this.toast.fire(this.toastMessage);
     }
 }
 
