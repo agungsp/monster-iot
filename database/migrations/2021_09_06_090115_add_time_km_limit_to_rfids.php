63df93e03ef9b57ac620bf71340c98238c797964
Agung Setyo Pribadi <agungsetyopribadi8@gmail.com>
2021-09-06 09:18:00 +0700

    Add migration for revision

---
+++ database/migrations/2021_09_06_090115_add_time_km_limit_to_rfids.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTimeKmLimitToRfids extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('rfids', function (Blueprint $table) {
+            $table->integer('time_limit')->nullable();
+            $table->integer('km_limit')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('rfids', function (Blueprint $table) {
+            $table->dropColumn(['time_limit', 'km_limit']);
+        });
+    }
+}
